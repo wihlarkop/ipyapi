a546abe6c8e7c1b0f7e0e657293a115a7ed208bb
LAPTOP-KH59JRMG\User <wihlarko.prasdegho@efishery.com>
2023-04-18 04:34:49 +0700

    feat: - add client

---
+++ client.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+import httpx
+
+from exception import QuotaExceeded
+from response_mapper import IpResponse
+
+
+class IPyAPI:
+    def __init__(self):
+        self.__base_url: str = "https://ipapi.co"
+        self.__client = httpx.Client()
+
+    def __del__(self):
+        self.__client.close()
+
+    def perform_request(self, url: str) -> httpx.Response:
+        response = self.__client.get(url)
+        if response.status_code == httpx.codes.TOO_MANY_REQUESTS:
+            raise QuotaExceeded("API quota exceeded")
+        response.raise_for_status()
+        return response
+
+    def get_client_ip(self, is_json: bool = True) -> Any | IpResponse:
+        url = f"{self.__base_url}/json"
+        response = self.perform_request(url=url)
+        if is_json:
+            return response.json()
+        else:
+            return IpResponse(**response.json())
